350b78221a29d1c836d78071b77afbed8816991a
Karen Langner <krenabeth@yahoo.com>
2016-07-18 22:50:29 -0400

    now locating reddit content

--- scripts.js
+++ scripts.js
@@ -1,18 +1,21 @@
-
 $(document).ready(function() { 
-	$.get('https://www.reddit.com/r/aww.json', function(reddit) {
+
+//  $('div.content').on(function() {
+//   $( 'this' ).toggle( "explode" );
+// });
+
+
+	$.get('http://www.reddit.com/r/aww.json', function(reddit) {
 // get the title, author, and preview for the first 10
 		var data = '<ul class="list">';
 		for (var i=0; i<10; i++) {
 			// var preview = reddit.data.children[i].preview.images[0]; // image need url or width and height?
 			// add the above to the html
-			data += '<li class="item"><h2>' + reddit.data.children[i].title + '</h2>';
-			data += '<h3>by ' + reddit.data.children[i].author + '</h3>' + reddit.data.children[i].thumbnail + '</li>';
+			data += '<li class="item"><h2>' + reddit.data.children[i].data.title + '</h2>';
+			data += '<h3>by ' + reddit.data.children[i].data.author + '</h3>' + '<img src=' + reddit.data.children[i].data.preview.images[0].source.url + ' class="pic"' + '/>' + '</li>';
 		}
 		data += '</ul>';
-		console.log(data);
-		$('div').html(data);
-
+		$('div.content').html(data);
   }); 
 });
 
