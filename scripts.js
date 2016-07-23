28d81df72f0bc3f21c8f2482003436565f92385e
Karen Langner <krenabeth@yahoo.com>
2016-07-22 21:10:41 -0400

    styling on the purple div

--- scripts.js
+++ scripts.js
@@ -1,10 +1,5 @@
 $(document).ready(function() { 
 
-//  $('div.content').on(function() {
-//   $( 'this' ).toggle( "explode" );
-// });
-
-
 	$.get('http://www.reddit.com/r/aww.json', function(reddit) {
 // get the title, author, and preview for the first 10
 		var data = '<ul class="list">';
@@ -12,7 +7,7 @@ $(document).ready(function() {
 			// var preview = reddit.data.children[i].preview.images[0]; // image need url or width and height?
 			// add the above to the html
 			data += '<li class="item"><h2>' + reddit.data.children[i].data.title + '</h2>';
-			data += '<h3>by ' + reddit.data.children[i].data.author + '</h3>' + '<img src=' + reddit.data.children[i].data.preview.images[0].source.url + ' class="pic"' + '/>' + '</li>';
+			data += '<h3>by ' + reddit.data.children[i].data.author + '</h3>' + '<a href=' + reddit.data.children[i].data.url + '>link</a></li>';
 		}
 		data += '</ul>';
 		$('div.content').html(data);
